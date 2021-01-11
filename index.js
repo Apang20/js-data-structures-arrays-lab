02274ec1cb0db91400b1db7494057064c269e594
github username <github email address>
2021-01-11 15:51:11 -0600

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
-// Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"];
+const drivers = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendDriver(name)
-{
-    drivers.push(name);    
+function destructivelyAppendDriver(new_driver) {
+  drivers.push(new_driver)
+}
+function destructivelyPrependDriver(new_driver) {
+  drivers.unshift(new_driver)
+  console.log(drivers)
 }
 
-function destructivelyPrependDriver(name)
-{
-    drivers.unshift(name);
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
+function destructivelyRemoveFirstDriver() {
+  drivers.shift()
 }
 
-function destructivelyRemoveLastDriver()
-{
-    drivers.pop()
-}
 
-function destructivelyRemoveFirstDriver()
-{
-    drivers.shift();
+function appendDriver(new_driver) {
+  const new_arr = drivers.slice(0)
+  new_arr.push(new_driver)
+  return new_arr
 }
-
-function appendDriver(name)
-{
-    return [...drivers, name];
+function prependDriver(new_driver) {
+  const new_arr = drivers.slice(0)
+  new_arr.unshift(new_driver)
+  return new_arr
 }
-
-function prependDriver(name)
-{
-    return [name, ...drivers];
+function removeLastDriver() {
+  const new_arr = drivers.slice(0)
+  new_arr.pop()
+  return new_arr
 }
-
-function removeLastDriver()
-{
-    return drivers.slice(0, drivers.length - 1);
+function removeFirstDriver() {
+  const new_arr = drivers.slice(0)
+  new_arr.shift()
+  return new_arr
 }
-
-function removeFirstDriver()
-{
-    return drivers.slice(1);
-} 
